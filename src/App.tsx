e18af12f1fd587c9a0d316753df2ed89fa9cb974
Takaaki-Shimizu <tattaka_feelmysoul@yahoo.co.jp>
2022-05-08 13:41:40 +0900

    API通信時loading処理追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Title from "./components/Title";
 import Form from "./components/Form";
 import Results from "./components/Results";
+import Loading from "./components/Loading";
 import "./App.css";
 
 type ResultsStateType = {
@@ -13,6 +14,7 @@ type ResultsStateType = {
 };
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(false);
   const [city, setCity] = useState<string>("");
   const [results, setResults] = useState<ResultsStateType>({
     country: "",
@@ -23,20 +25,22 @@ function App() {
   });
   const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
     fetch(
       `https://api.weatherapi.com/v1/current.json?key=f17dd03a2cdc45fc9be201826220305&q=${city}&aqi=no`
     ).then((res) =>
       res
         .json()
-        .then((data) =>
+        .then((data) => {
           setResults({
             country: data.location.country,
             cityName: data.location.name,
             temperature: data.current.temp_c,
             conditionText: data.current.condition.text,
             icon: data.current.condition.icon,
-          })
-        )
+          });
+          setLoading(false);
+        })
         .catch((err) =>
           alert("An error has occurred Please reload the page and try again.")
         )
@@ -47,7 +51,7 @@ function App() {
       <div className="container">
         <Title />
         <Form setCity={setCity} getWeather={getWeather} />
-        <Results results={results} />
+        {loading ? <Loading /> : <Results results={results} />}
       </div>
     </div>
   );
